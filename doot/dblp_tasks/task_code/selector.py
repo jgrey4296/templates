d030f4327430fa2adcf0bcec234ac11379f43c6f
John Grey <johngrey4296@gmail.com>
2024-01-26 18:22:32 +0000

    [add]: dblp journ/conf selector

--- doot/dblp_tasks/task_code/selector.py
+++ doot/dblp_tasks/task_code/selector.py
@@ -44,199 +44,118 @@ import doot.errors
 from doot.structs import DootKey
 from dootle.actions.xml import DootSaxHandler
 
-MAPPINGS = DootKey.make("mappings")
-PATTERNS = DootKey.make("patterns")
 UPDATE   = DootKey.make("update_")
 FROM_K   = DootKey.make("from")
-ENTRIES  = DootKey.make("entries")
 
-##-- regex
-skip_re = re.compile(r"(?i)dickin")
-
-##-- end regex
-
-def extract_entries(spec, state):
-    """ map files -> found entries """
-    mappings = MAPPINGS.to_type(spec, state)
+def extract_results(spec, state):
+    """  """
     source   = FROM_K.to_type(spec, state)
-    update   = UPDATE.redirect(spec)
-    results  = source.entries.copy()
+    journals = source._journals.copy()
+    confs    = source._conferences.copy()
+    jraw = source._journals_set.copy()
+    craw = source._conferences_set.copy()
 
-    printer.warning("-- Found %s results", len(results))
-    return { update : results }
+    printer.warning("Found %s journals, %s confs", len(journals), len(confs))
+    return { "journals": journals, "confs": confs, "jraw": jraw, "craw": craw}
 
-def flatten_results(spec, state):
-    """
-      merge {file -> [entry]} results,
-      and return them as a list of dicts ready for write.xml.entries of
-      [{ fpath: x, entries: [] }]
-
-    """
+def flatten_dicts(spec, state):
+    """ """
     source = FROM_K.to_type(spec, state)
     update = UPDATE.redirect(spec)
-    flat_mapping = defaultdict(list)
-    for mapping in source:
-        for key,val in mapping.items():
-            flat_mapping[key] += val
+    flat = {}
+    for d in source:
+        flat.update(d)
 
-    result = [{"fpath": x, "entries": y} for x,y in flat_mapping.items()]
-    printer.info("Flattened results to %s groups", len(result))
-    return { update : result }
+    printer.warning("Collected %s %s", len(flat), spec.kwargs['from_'])
+    result_str = "\n".join(f"{k:10} : {flat[k]}" for k in sorted(flat.keys()))
+    return { update : result_str }
 
-class DBLPHandler(DootSaxHandler):
+def flatten_raw(spec, state):
+    source = FROM_K.to_type(spec, state)
+    update = UPDATE.redirect(spec)
+    result_str = "\n".join(source)
+    return { update : result_str }
+
+class DBLPSelector(DootSaxHandler):
     """ A SAX handler for the DBLP dataset
       Assembles bibtexparser entries and fields from matching keys/crossref entries
     """
 
-    class _State(enum.Enum):
-        wait    = enum.auto()
-        active  = enum.auto()
-        check   = enum.auto()
+    class _StateE(enum.Enum):
+        wait       = enum.auto()
+        proceeding = enum.auto()
+        article    = enum.auto()
+        title      = enum.auto()
+        journal    = enum.auto()
+        check      = enum.auto()
 
     def __init__(self, spec, state):
-        # Get explicit key mapping
-        match MAPPINGS.to_type(spec, state, on_fail=[]):
-            case [dict() as x]:
-                self._mapping = x
-            case []:
-                self._mapping = {}
-                printer.info("No Mappings Found")
-                # raise doot.errors.DootActionError("No mappings found")
-            case [*xs]:
-                raise doot.errors.DootActionError("Too Many Mappings Found")
-
-        # Get pattern mapping
-        match PATTERNS.to_type(spec, state, on_fail=None):
-            case [dict() as x]:
-                self._patterns : dict[str, pl.Path]    = x
-                self._regexs   : dict[str, re.Pattern] = { k : re.compile(k) for k in x.keys() }
-            case _:
-                printer.info("No Patterns Found")
-                self._patterns = {}
-                self._regexs = {}
-                pass
         # Store entries connected to each key:
         self.entries : dict[pl.Path, list]   = defaultdict(list)
         # Search state
-        self._state      = DBLPHandler._State.wait
-        # Looking for entries:
-        self._entry_fields = ENTRIES.to_type(spec, state, type_=list, on_fail=["proceedings", "inproceedings"])
-        self._key_fields   = ["crossref"]
-        # field any characters relates to
-        self._curr_fields = []
-        # the current entry being created
-        self._curr_entry = None
-        # keys the current entry matches on
-        self._curr_keys       = []
-        self._suffix          = ".bib"
-        self._ignore_elements = ["i"]
+        self._state           = self._StateE.wait
+        self._curr_key        = ""
+        self._curr_content    = []
+        self._journals        = {}
+        self._conferences     = {}
+        self._journals_set    = set()
+        self._conferences_set = set()
+
 
     def startElement(self, name, attrs):
-        match self._state:
-            case DBLPHandler._State.wait if name.lower() in self._entry_fields:
-                self._state      = DBLPHandler._State.active
-                self._curr_entry = Entry(name, key=attrs['key'], fields=[])
-                self._curr_keys.append(attrs['key'])
-                if 'publtype' in attrs:
-                    self._curr_fields.append(Field("pubtype", attrs['publtype']))
-            case DBLPHandler._State.active if name in self._ignore_elements:
-                pass
-            case DBLPHandler._State.active:
-                self._curr_fields.append(Field(name, ""))
-            case DBLPHandler._State.wait:
-                pass
+        if 'key' in attrs:
+            key = pl.Path(attrs['key'])
+            self._curr_key = "/".join(key.parts[:-1]).strip()
+
+        match self._state, name.lower():
+            case self._StateE.wait, "proceedings":
+                self._state = self._StateE.proceeding
+            case self._StateE.wait, "journal":
+                self._state = self._StateE.journal
+            case self._StateE.proceeding, "title":
+                self._state = self._StateE.title
             case _:
-                raise doot.errors.DootTaskError("Shouldn't be able to get here", name)
+                pass
 
     def endElement(self, name):
         match self._state:
-            case DBLPHandler._State.wait:
+            case self._StateE.title if self._curr_key not in self._conferences:
+                self._state = self._StateE.wait
+                name = "".join(self._curr_content).replace("\n", " ").strip()
+                self._conferences[self._curr_key] = name
+                self._conferences_set.add(name)
+                # printer.warning("Storing Conf     : %s : %s", self._curr_key, self._conferences[self._curr_key])
+                self._clear()
                 pass
-            case DBLPHandler._State.active if name.lower() in self._entry_fields:
-                self._state = DBLPHandler._State.check
-                self._curr_entry.fields = self._curr_fields[:]
-            case DBLPHandler._State.active if name.lower() in self._key_fields:
-                self._curr_keys.append(self._curr_fields[-1].value)
-            case DBLPHandler._State.active:
+            case self._StateE.journal if self._curr_key not in self._journals:
+                self._state = self._StateE.wait
+                name = "".join(self._curr_content).replace("\n", " ").strip()
+                self._journals[self._curr_key] = name
+                self._journals_set.add(name)
+                # printer.warning("Storing: Journal : %s : %s", self._curr_key, self._journals[self._curr_key])
+                self._clear()
                 pass
+            case self._StateE.title:
+                self._state = self._StateE.wait
+                name = "".join(self._curr_content).replace("\n", " ").strip()
+                self._conferences_set.add(name)
+                self._clear()
+            case self._StateE.journal:
+                self._state = self._StateE.wait
+                name = "".join(self._curr_content).replace("\n", " ").strip()
+                self._journals_set.add(name)
+                self._clear()
             case _:
-                raise doot.errors.DootTaskError("Shouldn't be able to get here", name)
-
-        if self._state != DBLPHandler._State.check:
-            return
-
-        # See if this entry is one I want
-        self._maybe_store_entry()
-
-        self._state = DBLPHandler._State.wait
-        self._clear()
+                pass
 
     def characters(self, content):
         match self._state:
-            case _ if not bool(content.strip()):
+            case self._StateE.title | self._StateE.journal:
+                self._curr_content.append(content)
+            case _:
                 pass
-            case DBLPHandler._State.wait:
-                pass
-            case DBLPHandler._State.active if "https://doi.org" in content:
-                self._curr_fields[-1].key = "doi"
-                self._curr_fields[-1].value += content
-            case DBLPHandler._State.active if bool(self._curr_fields):
-                self._curr_fields[-1].value += content
 
     def _clear(self):
         """ Clear the current entry info """
-        self._curr_fields = []
-        self._curr_entry  = None
-        self._curr_keys   = []
-
-    def _maybe_store_entry(self):
-        # if theres an explicit key -> file mapping:
-        for field in self._curr_entry.fields:
-            if field.key.lower().strip() != "author":
-                continue
-
-            if skip_re.search(field.value):
-                self._clear()
-                return
-
-        match_keys = [y for x in self._curr_keys if (y:=self._mapping.get(x, None))]
-        if bool(match_keys):
-            self.entries[match_keys[0]].append(self._curr_entry)
-
-        match self._curr_entry.entry_type:
-            case "proceedings" | "inproceedings":
-                self._handle_proceedings()
-            case "article":
-                self._handle_article()
-
-    def _handle_proceedings(self):
-        for k in self._patterns:
-            regex = self._regexs[k]
-            base  = self._patterns[k]
-            if any(regex.match(x) for x in self._curr_keys):
-                if "crossref" in self._curr_entry.fields_dict:
-                    crossref = self._curr_entry.fields_dict['crossref'].value
-                    clean    = crossref.replace("/", "_").removeprefix("conf_")
-                    target   = base / clean
-                else:
-                    key    = self._curr_entry.key
-                    clean  = key.replace("/", "_").removeprefix("conf_")
-                    target = base / clean
-
-                self.entries[target.with_suffix(self._suffix)].append(self._curr_entry)
-
-
-    def _handle_article(self):
-        for k in self._patterns:
-            regex = self._regexs[k]
-            base  = self._patterns[k]
-            if any(regex.match(x) for x in self._curr_keys):
-                key    = self._curr_entry.key
-                match self._curr_entry.fields_dict.get("year", None):
-                    case None:
-                        year = "0000"
-                    case x:
-                        year = x.value
-                clean = key.split("/")[1] + "_" + year
-                target = base / clean
-                self.entries[target.with_suffix(self._suffix)].append(self._curr_entry)
+        self._curr_content = []
+        self._curr_key = ""

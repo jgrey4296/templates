55972830fbec34e4412f535b30924062860d49f7
John Grey <johngrey4296@gmail.com>
2024-05-15 22:05:57 +0100

    [chore]: dotnet

--- bash/components/dotnet.bash
+++ bash/components/dotnet.bash
@@ -1,7 +1,7 @@
 # Settings for dotnet / c#
 
 DOTNET_ROOT="/usr/lib/dotnet"
-NUGET_PACKAGES="$BASE_CACHE/nuget/packages"
+NUGET_PACKAGES="$BASE_CACHE/dotnet/packages"
 DOTNET_CLI_HOME="$BASE_CACHE/dotnet"
 
 PATH="$BASE_CACHE/dotnet/.dotnet/tools:$PATH"

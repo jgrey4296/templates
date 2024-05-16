60a7727cdfe1c7d672f6703771d03bafb719f4e6
John Grey <johngrey4296@gmail.com>
2024-05-16 23:12:47 +0100

    [chore]: dotnet path

--- bash/components/dotnet.bash
+++ bash/components/dotnet.bash
@@ -3,5 +3,6 @@
 DOTNET_ROOT="/usr/lib/dotnet"
 NUGET_PACKAGES="$BASE_CACHE/dotnet/packages"
 DOTNET_CLI_HOME="$BASE_CACHE/dotnet"
+DOTNET_BIN="/snap/dotnet-sdk/current/"
 
-PATH="$BASE_CACHE/dotnet/.dotnet/tools:$PATH"
+PATH="$BASE_CACHE/dotnet/.dotnet/tools:$DOTNET_BIN:$PATH"

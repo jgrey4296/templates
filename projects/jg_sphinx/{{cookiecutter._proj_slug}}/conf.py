9be6e7bb13e5e007e833afb6da2973ff4ad00f0c
John Grey <johngrey4296@gmail.com>
2025-02-17 21:58:06 +0000

    [add]: sphinx templates

--- projects/jg_sphinx/{{cookiecutter._proj_slug}}/conf.py
+++ projects/jg_sphinx/{{cookiecutter._proj_slug}}/conf.py
@@ -7,42 +7,20 @@ import os
 import sys
 import pathlib as pl
 
-from docutils import nodes
-from docutils.parsers.rst import directives
-from docutils.statemachine import StringList
-from docutils.transforms import Transform, TransformError
-from sphinx.locale import __
-from sphinx.util.docutils import SphinxDirective
+`(+snippet-expand "docutils.imports")
 
 # -- Project information -----------------------------------------------------
 
-release        = "0.13.0"
-project        = 'Doot'
+release        = "0.1.0"
+project        = 'TODO'
 author         = 'John Grey'
 copyright      = '{}, {}'.format(datetime.datetime.now().strftime("%Y"), author)
 primary_domain = "py"
 extensions = [
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.napoleon',
-    'sphinx.ext.extlinks',
-    'sphinx_rtd_theme',
-    'myst_parser',
-    "autoapi.extension",
-    "sphinx.ext.coverage",
-    "sphinx.ext.imgconverter",
-    "sphinx.ext.intersphinx",
-    "sphinx.ext.viewcode",
+    `(+snippet-expand "default.extensions")`
     ]
 
-def setup(app):
-    app.events.connect("builder-inited", add_jinja_ext, 1)
-    app.add_directive('jgdir', JGDirective)
-    # app.add_transform
-
-def add_jinja_ext(app):
-    app.builder.templates.environment.add_extension('jinja2.ext.debug')
+`(+snippet-expand "jinja.ext")
 
 # -- Path setup --------------------------------------------------------------
 
@@ -67,7 +45,7 @@ master_doc                    = "index"
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['**/flycheck_*.py', "**/__tests/*"]
+exclude_patterns = ['**/flycheck_*.py', "**/__tests/*", '/obsolete/*', "README.md"]
 
 # suppress_warnings = ["autoapi", "docutils"]
 
@@ -80,46 +58,7 @@ master_doc                    = "index"
 show_warning_types            = True
 
 # -- Options for HTML output -------------------------------------------------
-# https://sphinx-rtd-theme.readthedocs.io/en/stable/configuring.html
-html_theme         = "sphinx_rtd_theme"
-html_theme_options = {}
-html_sidebars      = {}
-html_domain_indices = True
-html_use_index      = True
-
-html_theme_options.update({
-    'logo_only'                   : False,
-    'display_version'             : True,
-    'prev_next_buttons_location'  : 'bottom',
-    'style_external_links'        : False,
-    'vcs_pageview_mode'           : '',
-    'style_nav_header_background' : 'grey',
-    # TOC options:
-    'collapse_navigation'         : True,
-    'sticky_navigation'           : True,
-    'navigation_depth'            : 4,
-    'includehidden'               : True,
-    'titles_only'                 : False
-
-})
+`(+snippet-expand "rtd.options")`
 
 # -- Extension Options -------------------------------------------------
-# https://sphinx-autoapi.readthedocs.io/en/latest/reference/config.html
-autoapi_generate_api_docs = True
-autoapi_add_toctree_entry = True
-autoapi_type              = "python"
-autoapi_template_dir      = "_templates"
-autoapi_root              = "autoapi"
-autoapi_dirs              = ['../doot']
-autoapi_file_patterns     = ["*.py", "*.pyi"]
-autoapi_ignore            = ['*/__tests', '*/test_*.py', '/obsolete/*']
-autoapi_options           = [
-    'imported-members',
-    'members',
-    # 'undoc-members',
-    'private-members',
-    'special_members',
-    'show-inheritance',
-    # 'show-inheritance-diagram',
-    # 'show-module-summary',
-]
+`(+snippet-expand "autoapi.options")`

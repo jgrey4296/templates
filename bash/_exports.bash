b9e91b35b0e7e9d66d3460dce988f2f42c858b98
John Grey <johngrey4296@gmail.com>
2025-04-28 00:01:25 +0100

    [chore]: format bash exports

--- bash/_exports.bash
+++ bash/_exports.bash
@@ -2,62 +2,84 @@
 
 jgdebug "Setting Exports"
 
+# Personal
+export JGLOGDIR
+
+# General
 export HISTFILE="$BASE_CACHE/logs/bash_history"
 export LESSHISTFILE="$BASE_CACHE/logs/lesshst"
 export NODE_REPL_HISTORY="$BASE_CACHE/logs/node_repl_history"
-
-# To get rid of atk-bridge complaints:
-export G_MESSAGES_PREFIXED=""
-
+export G_MESSAGES_PREFIXED="" # To get rid of atk-bridge complaints
 export PATH
 export EDITOR
 export PROMPT_NUM
+export TERM
+export GPG_TTY
+export MANPATH
+export BASH_ENV
+export GNUPGHOME
+export GTAGSLABEL
+export BASH_SILENCE_DEPRECATION_WARNING=1
+
+# JVM
 export ANDROID_HOME
 export ANDROID_USER_HOME
 export ADB_VENDOR_KEYS
 export JDK_HOME
 export JAVA_HOME
-export TERM
+export JACAMO_HOME
+export GRADLE_USER_HOME
+export SDKMAN_DIR
+export SOAR_HOME
+export CLIPS_HOME
+
+# Emacs
 export EMACSDIR
 export DOOMDIR
-export JGLOGDIR
+
+# CMake
 export CMAKE_BUILD_PARALLEL_LEVEL
-export GPG_TTY
-export JACAMO_HOME
-export MANPATH
-export BASH_ENV
-export GTAGSLABEL
+export CMAKE_BUILD_PARALLEL_LEVEL
+export make_jobs
+
+# Python
 export ANACONDA_HOME
 export DYLD_FALLBACK_LIBRARY_PATH
 export PYTHONSTARTUP
+export PRE_COMMIT_USE_MICROMAMBA
+export PIPX_HOME
+export IPYTHONDIR
+export USE_EMOJI
+
+# Haskell
 export CABAL_CONFIG
 export CABAL_DIR
 export CARGO_HOME
+
+# Ocaml
 export OPAM_SWITCH_PREFIX
 export CAML_LD_LIBRARY_PATH
 export OCAML_TOPLEVEL_PATH
-export GRADLE_USER_HOME
+export CEPTRE_HOME
+
+# Rust
 export RUSTUP_HOME
 export CARGO_HOME
-export SDKMAN_DIR
-export GNUPGHOME
+
+# Mail
 export MUHOME
 export MAILDIR
-export SOAR_HOME
-export CLIPS_HOME
-export CEPTRE_HOME
-export make_jobs
-export CMAKE_BUILD_PARALLEL_LEVEL
+export MBOX
+
+# Dotnet
 export DOTNET_ROOT
 export NUGET_PACKAGES
 export DOTNET_CLI_HOME
-export ASDF_DATA_DIR
-export PRE_COMMIT_USE_MICROMAMBA
-export MBOX
-export PIPX_HOME
-export USE_EMOJI
-export BASH_SILENCE_DEPRECATION_WARNING=1
 
+# Elixir
+export ASDF_DATA_DIR
+
+# OS Type
 case "$OSTYPE" in
     darwin*)
         export HOMEBREW_MAKE_JOBS=1
@@ -67,8 +89,7 @@ case "$OSTYPE" in
         ;;
 esac
 
-# export TMPDIR
-
+# Functions
 export -f jgdebug
 export -f jg_maybe_inc_prompt
 export -f jg_prompt_update

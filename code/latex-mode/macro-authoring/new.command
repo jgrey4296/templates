60eee8d11d3ca67be375c202d18c5b7a8ef78c65
John Grey <johngrey4296@gmail.com>
2025-09-27 13:56:48 +0100

    [refactor]: latex templates

---
+++ code/latex-mode/macro-authoring/new.command
@@ -0,0 +1,9 @@
+# -*- mode: snippet -*-
+# name: new.cmd
+# key: new.cmd
+# uuid: new.cmd
+# group: new macro
+# --
+\newcommand*{\\${1:name}}[${2:0}]{
+    $0
+}
---
+++ code/latex-mode/macro-authoring/new.robust.cmd
@@ -0,0 +1,9 @@
+# -*- mode: snippet -*-
+# name  : new.robust.cmd
+# uuid  : new.robust.cmd
+# key   : new.robust.cmd
+# group : new macros
+# --
+\DeclareRobustCommand[\\$1][${2:0}]{
+    $0
+}

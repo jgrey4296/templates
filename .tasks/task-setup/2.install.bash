b166acd0b04d866a4f9a226fe0c038da3a29a7f4
John Grey <johngrey4296@gmail.com>
2025-12-08 20:39:28 +0000

    [chore]:

---
+++ .tasks/task-setup/2.install.bash
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+# 2.install.bash -*- mode: sh -*-
+#set -o errexit
+set -o nounset
+set -o pipefail
+
+snap install emacs --classic
+snap install libreoffice remmina vivaldi vlc ghidra
+
+sudo apt install vim neovim clamav csound graphviz imagemagick jackd
+sudo apt install supercollider lldb tesseract-ocr tmux tree-sitter-cli
+sudo apt install trash-cli z3 golang-go fd-find gh curl ripgrep
+sudo apt install build-essential autoconf m4 libwxgtk3.2-dev libwxgtk-webview3.2-dev
+sudo apt install libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev xsltproc fop libxml2-utils libncurses-dev openjdk-11-jdk
+sudo apt install btop calibre cmake
+
+# from https://www.swi-prolog.org/build/Debian.html
+sudo apt install build-essential cmake ninja-build pkg-config
+sudo apt install ncurses-dev libedit-dev libgoogle-perftools-dev
+sudo apt install libgmp-dev libssl-dev unixodbc-dev
+sudo apt install zlib1g-dev libarchive-dev libossp-uuid-dev
+sudo apt install libdb-dev libpcre3-dev libyaml-dev
